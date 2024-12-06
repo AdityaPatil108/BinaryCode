6c0f11868b95911276bb52a0cb1c79eb17b64bcd
Adityazingade896 <adityazingade.896@gmail.com>
2024-12-06 18:04:24 +0530

    changes done

---
+++ Number to Binery.js
@@ -0,0 +1,8 @@
+function numberToBinary(number) {
+    return number.toString(2);
+}
+
+// Example usage
+let input = document.getElementById('.'); // You can replace this with any number
+let binaryOutput = numberToBinary(input);
+console.log(binaryOutput);
